262e0bdb66be715285a91c6dd59931b2e9efa5fb
Krishna Pavan <133009685+kpdev96@users.noreply.github.com>
2025-12-26 15:16:57 +0530

    region change

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -7,13 +7,13 @@ variable "project" {
 variable "region" {
   description = "this is gcp region"
   type        = string
-  default     = "africa-south1"
+  default     = "asia-south1"
 }
 
 variable "zone" {
   description = "this is gcp zone"
   type        = string
-  default     = "africa-south1-a"
+  default     = "asia-south1-a"
 }
 
 variable "K8s_version" {
